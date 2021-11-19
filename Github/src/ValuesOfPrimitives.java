64e3005834d186c7fec7beab66cd4dd4ae912c2a
E S <tallboy6077@gmail.com>
2021-11-20 00:00:39 +0300

    How to find Min and Max values of Primitive data types in Eclipse.

---
+++ Github/src/ValuesOfPrimitives.java
@@ -0,0 +1,21 @@
+
+public class ValuesOfPrimitives {
+
+	public static void main(String[] args) {
+		// How to find MIN and MAX values of Primitive data types in Eclipse?
+		
+		int num1 = Integer.MIN_VALUE;
+		System.out.println(num1);
+		
+		int num2 = Integer.MAX_VALUE;
+		System.out.println(num2);
+		
+		int num3 = Byte.MIN_VALUE;
+		System.out.println(num3);
+ 
+		int num4 = Byte.MAX_VALUE;
+		System.out.println(num4);
+		
+	}
+
+}
