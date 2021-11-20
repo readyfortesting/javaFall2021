4350482dd53dec05b6ed0096f4df51eb4d21fbfb
Burak Akyuz <tallboy6077@gmail.com>
2021-11-20 14:19:51 +0300

    Ask user to their weight,height type a prog with calculates mass index

---
+++ Github/src/VucutKitleIndeksi.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class VucutKitleIndeksi {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Insert your weight please");
+		double weight=scan.nextDouble();
+
+		System.out.println("Insert your  height please");
+		double height= scan.nextDouble();
+
+		double bmi=weight/(height*height);
+
+		if (bmi<=18.5) {
+		  System.out.println("You are weak");
+		} else if (bmi<=30) {
+		  System.out.println("Your weight is ideal");
+		} else if (bmi>30) {
+			System.out.println("I am sorry, you are fat");
+		}{
+		  System.out.println("bmi" + bmi);
+		} {
+		  
+		}
+
+	}
+
+}
