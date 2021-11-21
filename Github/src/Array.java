3815c2b1d83756d9109b01d2890d893345e71c27
Burak Akyuz <tallboy6077@gmail.com>
2021-11-21 17:18:33 +0300

    Create an Array

---
+++ Github/src/Array.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// Create an array and have 5 different names in the array you created.
+		
+		String [] array = new String[5];
+		
+		        array[0] ="Ali Esat";
+				array[1] = "Oglum";
+				array[2] ="Balkupum";
+				array[3] ="Emanetim";
+				array[4] = "Evlat";
+				
+				System.out.println(Arrays.toString(array));
+
+	}
+
+}
---
+++ Github/src/isimsoyisim.java
@@ -0,0 +1,18 @@
+
+public class isimsoyisim {
+
+	public static void main(String[] args) {
+		
+		char ch = 'a';
+
+		while(ch<'d') {
+
+				System.out.print(ch + " ");
+
+				ch++;
+
+		}
+}
+	}
+
+
