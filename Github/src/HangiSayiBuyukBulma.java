d874badc9476820f61249fbcfd7ebd39d41f808c
Burak Akyuz <tallboy6077@gmail.com>
2021-11-23 13:25:15 +0300

    Write a program that finds which number is greater than two numbers.

---
+++ Github/src/HangiSayiBuyukBulma.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class HangiSayiBuyukBulma {
+
+	public static void main(String[] args) {
+		
+		//İki sayıdan hangi sayının büyük olduğunu bulan bir program yazınız.
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Input first number please");
+		int number1=scan.nextInt();
+		
+		
+		System.out.println("Input second number please");
+		int number2=scan.nextInt();
+		
+		// Simdi bu iki sayiyi karsilastirmamiz gerekir. 3 farkli kosul var.
+		
+		//1.kosul
+		if (number1>number2) {
+			System.out.println("First number is greater");
+		}else if (number2>number1) {
+			System.out.println("Second number is greater");
+		}else {
+			System.out.println("Numbers are equal");
+		}
+		
+
+	}
+
+}
