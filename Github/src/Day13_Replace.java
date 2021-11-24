88b6f3ca8c1f885f2d00a4d56d4d0ae6b3ae2984
Burak Akyuz <tallboy6077@gmail.com>
2021-11-24 13:57:30 +0300

    day13_REPLACE

---
+++ Github/src/Day13_Replace.java
@@ -0,0 +1,60 @@
+
+
+public class Day13_Replace {
+
+	public static void main(String[] args) {
+		
+		// String olarak verilen 41.000 sayisinin binden buyuk olup
+		// olmadigini yazdirin.
+		
+		// String olarak verilen 41.000 sayisinin
+        // binden buyuk olup olmadigini yazdirin
+
+		
+		
+		 //	String sonuc="41.000";
+		
+		 //	sonuc=sonuc.replace("." ," ");  // " ." Noktayý " " hiclik ile degistir komutu veriyoruz.
+		
+		 //	if (Integer.valueOf(sonuc)>15000) {
+		 //		System.out.println("bulunan sonuc 15000'den cok");
+		 //	} else {
+		 //       System.out.println("bulunan sonuc 15000'den az");
+		 //	}
+		
+		
+		
+		
+		
+		String str= "Java ogrenmek mesakkatli bir is"; // Bosluklari kaldirmak icin 
+		
+		System.out.println(str.replace(" ", "")); // " ", bosluk yerine "" hiclik.
+		
+		System.out.println(str.replace("ogrenmek" , "kod yazmak")); // Java kod yazmak cok zevkli
+		
+		System.out.println(str.replace("k", "d")); // JAVA OGRENMED MESADDATLÝ BÝR ÝS
+		
+		//**** replace METODU CHAR ICIN DE KULLANÝLABÝLÝR.
+		
+		// **** replaceAll metodunda char kullanilmaz *****
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
