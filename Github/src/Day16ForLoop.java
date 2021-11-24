d6e17494642e306332bd5987de22d1a88a587496
Burak Akyuz <tallboy6077@gmail.com>
2021-11-24 14:02:48 +0300

    day16 FOR LOOP

---
+++ Github/src/Day16ForLoop.java
@@ -0,0 +1,26 @@
+
+
+public class Day16ForLoop {
+
+	public static void main(String[] args) {
+		// 
+		
+		
+		for (int i = 4; i >1; i--) {
+			System.out.println(i);
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
