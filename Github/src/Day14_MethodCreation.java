8ca7788c2621b300ef03deea40e7cd5b87ded6d6
Burak Akyuz <tallboy6077@gmail.com>
2021-11-24 14:01:49 +0300

    Method Creation

---
+++ Github/src/Day14_MethodCreation.java
@@ -0,0 +1,51 @@
+
+
+import java.util.Scanner;
+
+public class Day14_MethodCreation {
+
+	public static void faktoriyel () {
+		
+	Scanner scan = new Scanner (System.in);
+	System.out.println("Sayi:");
+	
+	int sayi= scan.nextInt();
+	int faktoriyel =1;
+	
+	while (sayi > 0) {
+		faktoriyel*=sayi;
+		sayi--;
+		
+		
+		
+	}
+	
+	System.out.println("Faktoriyel : " + faktoriyel);
+		
+	
+		
+		
+		
+		
+		
+		
+	}
+	public static void main(String[] args) {
+		
+		
+
+		faktoriyel();
+		faktoriyel();
+		faktoriyel();
+		faktoriyel();
+		faktoriyel();
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
