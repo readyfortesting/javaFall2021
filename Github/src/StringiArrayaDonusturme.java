90b64def6da06b015ca2ff56cb0b8f28913ac8a9
Burak Akyuz <tallboy6077@gmail.com>
2021-11-24 19:53:38 +0300

    Convert String to Array

---
+++ Github/src/StringiArrayaDonusturme.java
@@ -0,0 +1,22 @@
+
+public class StringiArrayaDonusturme {
+
+	public static void main(String[] args) {
+
+     String languages="English,Turkish,German,French,Russian";
+     
+     String[] array=languages.split(",");
+
+   System.out.println(array[0]);     
+   System.out.println(array[1]);     
+   System.out.println(array[2]);     
+   System.out.println(array[3]);     
+   System.out.println(array[4]);     
+    
+   
+   System.out.println(array.length); // 5
+   
+   
+	}
+
+}
