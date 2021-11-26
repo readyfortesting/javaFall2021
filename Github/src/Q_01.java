700fe0e264ecb3d3d24524a86a531ce59ff94899
Burak Akyuz <tallboy6077@gmail.com>
2021-11-26 22:21:16 +0300

    12345 sayısının rakamlar değerlerini yukarıdan aşağıya doğru yazdırınız

---
+++ Github/src/Q_01.java
@@ -0,0 +1,67 @@
+
+
+public class Q_01 {
+
+	public static void main(String[] args) {
+	
+		
+		//SORU 1)  verilen 12345 sayýsýnýn rakamlar deðerlerini yukarýdan aþaðýya doðru yazdýrýnýz
+				// ex: input: 
+//				    output: 1 // BÝRLER BASAMAÐINI BULMAK ÝÇÝN modulus KULLANILIR.
+//				            2
+//				            3
+//				            4
+//				            5
+				
+				int sayi=12345;
+				int birler= sayi%10; //birler basamagi sayinin 10 a bolumunden kalanýdýr.--> 5
+				int onlar=(sayi/10)%10;
+				int yuzler=(sayi/100)%10;
+				int binler=(sayi/1000)%10;
+				int onBinler=(sayi/10000);
+				
+			//	System.out.println(onBinler);
+				//System.out.println(binler);
+				//System.out.println(yuzler);    //bu 5 satýr yerine TEK 1 SATIRDA YAZABÝLÝRÝZ.
+				//System.out.println(onlar);
+				//System.out.println(birler);
+				
+				
+				/*
+			    System.out.println(binler);
+				System.out.println(yuzler);
+				System.out.println(onlar);
+				System.out.println(birler);
+				 */
+				
+				// consolda alt satira yazdýrmak için "\n"  komutu kullanýlýr.
+											
+		System.out.println(onBinler+ "\n" + binler+"\n"+ yuzler+"\n" + onlar +"\n" + birler);  // tek bir surada ters slash n \n ile yazdýk.
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
