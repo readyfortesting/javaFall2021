59e7456d51386a6ea66be006cb8876c5104f88ee
Burak Akyuz <tallboy6077@gmail.com>
2021-11-28 19:40:02 +0300

    Sum of first 100 natural numbers

---
+++ Github/src/SumOfNaturalNumbers.java
@@ -0,0 +1,17 @@
+
+public class SumOfNaturalNumbers {
+
+	public static void main(String[] args) {
+		//
+		
+	int num=5, count=100,total=0;
+	
+	while(count<=num)
+	{
+		total=total+count;
+		count++;
+	}
+	System.out.println("sum of the first 100 natural numbers is : " + total);
+}
+	
+}
