16be9149d0d4bb6bef0076ff2fdb7c4b2172f8ee
Burak Akyuz <tallboy6077@gmail.com>
2021-11-30 16:00:46 +0300

    ALFABE yazdirma

---
+++ Github/src/AlfabeYazdirma.java
@@ -0,0 +1,14 @@
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class AlfabeYazdirma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (char a = 'A'; a <='Z'; a++) {
+			System.out.print(" " + a);
+		}
+	} 
+
+}
