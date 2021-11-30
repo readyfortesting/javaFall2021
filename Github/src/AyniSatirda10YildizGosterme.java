7816bb009a2cd80a3f2f47a5b2ac4d4658f104b3
Burak Akyuz <tallboy6077@gmail.com>
2021-11-30 16:39:30 +0300

    Ayni satirda 10 * yazdirma

---
+++ Github/src/AyniSatirda10YildizGosterme.java
@@ -0,0 +1,15 @@
+
+public class AyniSatirda10YildizGosterme {
+
+	public static void main(String[] args) {
+		// Ayni satirda 10 yildiz gosterme
+		
+		 
+		for(int i=0; i<10; i++){
+		    System.out.print(" " + "*");
+		}
+		 } 
+		 }
+	
+
+
