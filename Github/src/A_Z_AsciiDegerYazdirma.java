3afe61a24c188316da9c9ec6ae221607456e4bfd
Burak Akyuz <tallboy6077@gmail.com>
2021-12-01 15:44:59 +0300

    A-Z ASCII YAZDIRMA

---
+++ Github/src/A_Z_AsciiDegerYazdirma.java
@@ -0,0 +1,13 @@
+
+public class A_Z_AsciiDegerYazdirma {
+
+	public static void main(String[] args) {
+		//A-Z ASCII deger yazdirma
+		
+		 for(char ch='A';ch<='Z';ch++)
+	        {
+	            System.out.println("The ASCII value of "+ch+" is "+(int)ch);
+	}
+
+}
+}
