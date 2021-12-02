6bb07cb9ee5788002dde927d60af13d5c62d51ba
Burak Akyuz <tallboy6077@gmail.com>
2021-12-03 00:20:39 +0300

    VARARGS

---
+++ Github/src/C06_Varargs.java
@@ -0,0 +1,42 @@
+
+
+public class C06_Varargs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10;
+		int b=20;
+		int c=30;
+		int d=40;
+		int e=50;
+		int f=60;
+		
+		topla(a,b);
+		topla(a,b,c);
+		topla(a,b,c,d);
+		topla(a,b,c,d,e);
+		topla(a,b,c,d,e,f);
+		
+		/*
+		 * suana kadar gordugumuz tekniklerle degisken sayida parametreli tek bir method olusturamayiz		
+		 */
+		
+	}
+
+	private static void topla(int...a) {
+		// TODO Auto-generated method stub
+		System.out.println("varargs calisir");
+	}
+
+	private static void topla(int a, int b, int c) {
+		// TODO Auto-generated method stub
+		System.out.println("uc sayinin toplami : " + (a+b+c));
+	}
+
+	private static void topla(int a, int b) {
+		// TODO Auto-generated method stub
+		System.out.println("iki sayinin toplami : " + (a+b));
+	}
+
+}
