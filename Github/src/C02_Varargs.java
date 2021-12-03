770539b23324479e42d3f15ed4deacbb16e4cc0b
Burak Akyuz <tallboy6077@gmail.com>
2021-12-03 23:27:39 +0300

    VARARGS

---
+++ Github/src/C02_Varargs.java
@@ -0,0 +1,23 @@
+
+
+public class C02_Varargs {
+
+	public static void main(String[] args) {
+
+		topla(5,8,10,10,13,0,15);
+		
+//******* VARARGS List gibi davranir fakat ARRAY methodlari gibi calisir.  ***********/////
+		
+		
+	}
+
+	private static void topla(int...sayilar) {
+		int toplam=0;
+		for (int each : sayilar) { // sayilar varargs'ina git ordaki herbir each'i bana getir
+			toplam+=each;
+			
+		}
+		System.out.println("girilen sayilarin toplami :" + toplam); // girilen sayilarin toplami :61
+	}
+
+}
