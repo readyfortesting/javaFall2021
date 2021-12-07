68bfb474306b32f30e38e65e58d2e53395a8e4f1
Burak Akyuz <tallboy6077@gmail.com>
2021-12-07 16:34:06 +0300

    Day03_Scanner_C05

---
+++ Github/src/day03_Scanner/C05.java
@@ -0,0 +1,34 @@
+package day03_Scanner;
+
+public class C05 {
+
+    int asgariMaas=4000;
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+
+	}
+
+	public int getAsgariMaas() {
+		return asgariMaas;
+	}
+
+	public void setAsgariMaas(int asgariMaas) {
+		this.asgariMaas = asgariMaas;
+	}
+	
+	/* Bir class'daki herhangi bir class uyesine ulasilabilsin ama degistirilemesin diyorsaniz 
+	 * 1- class uyesini PRIVATE yapin ( private yapinca hic ulasilmaz)
+	 * 2- private class uyelerine hicbir sekilde ulasim olmayacagi icin sadece gorebilme yetkisi verin.
+	 * ( getter() methodu ile )**
+	 * 
+	 * 
+	 * 
+	 */
+	
+	
+
+}
