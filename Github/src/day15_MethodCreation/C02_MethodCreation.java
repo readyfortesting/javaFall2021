6150bc108d43db89d55eef7b54bafd2061ad2834
Burak Akyuz <tallboy6077@gmail.com>
2021-12-11 16:30:29 +0300

    DAY15_METHOD CREATION

---
+++ Github/src/day15_MethodCreation/C02_MethodCreation.java
@@ -0,0 +1,10 @@
+package day15_MethodCreation;
+
+public class C02_MethodCreation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Github/src/day15_MethodCreation/H03_MethodCreation.java
@@ -0,0 +1,10 @@
+package day15_MethodCreation;
+
+public class H03_MethodCreation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
