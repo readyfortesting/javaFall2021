ea22d584153d2da275da4e99e9a65200d6ce8717
Burak Akyuz <tallboy6077@gmail.com>
2021-12-13 16:02:15 +0300

    Day21_Scope_Arrays

---
+++ Github/src/Day21ScopeArrays/C02_LoopVariables.java
@@ -0,0 +1,10 @@
+package Day21ScopeArrays;
+
+public class C02_LoopVariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Github/src/Day21ScopeArrays/C03_Arrays.java
@@ -0,0 +1,17 @@
+package Day21ScopeArrays;
+
+public class C03_Arrays {
+
+	public static void main(String[] args) {
+		
+		String arr[]=new String[3];
+		arr[0]="Ali";
+		arr[1]="Veli";
+		arr[2]="Ayse";
+		arr[3]="Fatma";
+		
+		String arr2[]= { "Ali","Veli","Ayse","Fatma" };
+
+	}
+
+}
