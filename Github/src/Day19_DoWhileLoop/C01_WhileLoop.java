53f13fac9dcf66abe36e9b985a3585944de26940
Burak Akyuz <tallboy6077@gmail.com>
2021-12-13 15:58:04 +0300

    Day19_WhileLoop_ForLoop_DoWhileLoop

---
+++ Github/src/Day19_DoWhileLoop/C01_WhileLoop.java
@@ -0,0 +1,10 @@
+package Day19_DoWhileLoop;
+
+public class C01_WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Github/src/Day19_DoWhileLoop/C04_DoWhileLoop.java
@@ -0,0 +1,31 @@
+package Day19_DoWhileLoop;
+
+public class C04_DoWhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int i=0;
+		
+		while(i<5) {
+			System.out.print(i + " "); // 0 1 2 3 4 
+			i++;
+		}
+		
+		int a=0;
+		do {
+			System.out.println(a + " ");
+			a++;
+		} while (a<5); //
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
