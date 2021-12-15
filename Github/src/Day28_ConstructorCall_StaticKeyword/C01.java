cac0e81c4343cf49aa964c3d6bd707e42b5c5241
Burak Akyuz <tallboy6077@gmail.com>
2021-12-15 22:41:48 +0300

    Day28_ConstructorCall_StaticKeyword

---
+++ Github/src/Day28_ConstructorCall_StaticKeyword/C01.java
@@ -0,0 +1,6 @@
+package Day28_ConstructorCall_StaticKeyword;
+
+public class C01 {
+
+	public static void main(String[] args) {}
+	}
---
+++ Github/src/Day28_ConstructorCall_StaticKeyword/C02.java
@@ -0,0 +1,16 @@
+package Day28_ConstructorCall_StaticKeyword;
+
+public class C02 {
+
+	
+	int x=5;
+	static int y=10;
+	
+	public static void main(String[] args) {
+		//3 tane obje olusturalim ve degisimleri gozlemleyelim.
+		C02 obje1=new C02();
+		System.out.println("obje1 icin x :" + obje1.x+"obje1 icin y :" + obje1.y);
+
+	}
+
+}
