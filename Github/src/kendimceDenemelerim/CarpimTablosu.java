163230b371a09c4c3e9d797a33f25551a512ec07
Burak Akyuz <tallboy6077@gmail.com>
2021-12-17 17:54:02 +0300

    Write a program that prints the multiplication table.

---
+++ Github/src/kendimceDenemelerim/CarpimTablosu.java
@@ -0,0 +1,80 @@
+package kendimceDenemelerim;
+
+public class CarpimTablosu {
+
+	public static void main(String[] args) {
+		// Write a program that prints the multiplication table. Use ++ operator.
+		
+		//STANDART OUTPUT
+		System.out.println("1x1=1");
+		System.out.println("1x2=2");
+		System.out.println("1x3=3");
+		System.out.println("1x4=4");
+		System.out.println("1x5=5");
+		System.out.println("1x6=6");
+		System.out.println("1x7=7");
+		System.out.println("1x8=8");
+		System.out.println("1x9=9");
+		System.out.println("1x10=10");
+		
+		//DYNAMIC OUTPUT
+		
+		int s1=1;
+		int s2=1;
+		
+		System.out.println(s1 + "x" + s2 + "=" + s1*s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1*s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1*s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1*s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1*s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1*s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1*s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1*s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1*s2++);
+		System.out.println(s1 + "x" + s2 + "=" + s1*s2++);
+		
+		int s3=2;
+		int s4=1;
+		
+		System.out.println(s3 + "x" + s4 + "=" + s3*s4++);
+		System.out.println(s3 + "x" + s4 + "=" + s3*s4++);
+		System.out.println(s3 + "x" + s4 + "=" + s3*s4++);
+		System.out.println(s3 + "x" + s4 + "=" + s3*s4++);
+		System.out.println(s3 + "x" + s4 + "=" + s3*s4++);
+		System.out.println(s3 + "x" + s4 + "=" + s3*s4++);
+		System.out.println(s3 + "x" + s4 + "=" + s3*s4++);
+		System.out.println(s3 + "x" + s4 + "=" + s3*s4++);
+		System.out.println(s3 + "x" + s4 + "=" + s3*s4++);
+		System.out.println(s3 + "x" + s4 + "=" + s3*s4++);
+		
+		int s5=3;
+		int s6=1;
+		
+		System.out.println(s5 + "x" + s6 + "=" + s5*s6++);
+		System.out.println(s5 + "x" + s6 + "=" + s5*s6++);
+		System.out.println(s5 + "x" + s6 + "=" + s5*s6++);
+		System.out.println(s5 + "x" + s6 + "=" + s5*s6++);
+		System.out.println(s5 + "x" + s6 + "=" + s5*s6++);
+		System.out.println(s5 + "x" + s6 + "=" + s5*s6++);
+		System.out.println(s5 + "x" + s6 + "=" + s5*s6++);
+		System.out.println(s5 + "x" + s6 + "=" + s5*s6++);
+		System.out.println(s5 + "x" + s6 + "=" + s5*s6++);
+		System.out.println(s5 + "x" + s6 + "=" + s5*s6++);
+		
+		int s7=4;
+		int s8=1;
+		
+		System.out.println(s7 + "x" + s8 + "=" + s7*s8++);
+		System.out.println(s7 + "x" + s8 + "=" + s7*s8++);
+		System.out.println(s7 + "x" + s8 + "=" + s7*s8++);
+		System.out.println(s7 + "x" + s8 + "=" + s7*s8++);
+		System.out.println(s7 + "x" + s8 + "=" + s7*s8++);
+		System.out.println(s7 + "x" + s8 + "=" + s7*s8++);
+		System.out.println(s7 + "x" + s8 + "=" + s7*s8++);
+		System.out.println(s7 + "x" + s8 + "=" + s7*s8++);
+		System.out.println(s7 + "x" + s8 + "=" + s7*s8++);
+		System.out.println(s7 + "x" + s8 + "=" + s7*s8++); 
+		
+	}
+
+}
