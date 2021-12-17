a606ce512426e1431306eaaa593c45d9e14e0350
Burak Akyuz <tallboy6077@gmail.com>
2021-12-17 21:28:00 +0300

    Day32_Varargs_StringBuilder

---
+++ Github/src/Day32_varargs_StringBuilder/C01_Varargs.java
@@ -0,0 +1,10 @@
+package Day32_varargs_StringBuilder;
+
+public class C01_Varargs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Github/src/Day32_varargs_StringBuilder/C06_StringBuilder.java
@@ -0,0 +1,14 @@
+package Day32_varargs_StringBuilder;
+
+public class C06_StringBuilder {
+
+	public static void main(String[] args) {
+		
+		String str="Ali Can";
+		String str2= new String ("Veli Cem");
+		
+	
+
+	}
+
+}
